60bf137cfc3c774f878129a4a60c018e6c1dc8bf
Alson Shareef <alson.shareef@gmail.com>
2019-08-23 19:58:40 +0800

    Contacts page

---
+++ src/pages/contact.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+
+const Contact = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Contact</h1>
+            <a
+                href="https://github.com/alsonshareef"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Github
+            </a>
+            <Footer />
+        </div>
+    );
+};
+
+export default Contact;
