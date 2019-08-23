321a4b43500a461a0d41a6885b75be7ced5bb4a4
Alson Shareef <alson.shareef@gmail.com>
2019-08-23 19:58:24 +0800

    Blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+
+const Blog = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Blog</h1>
+            <Footer />
+        </div>
+    );
+};
+
+export default Blog;
