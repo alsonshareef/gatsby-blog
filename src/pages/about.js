5221df1a37fad67a7078008a69de3669e758c576
Alson Shareef <alson.shareef@gmail.com>
2019-08-23 19:58:10 +0800

    About page

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+
+const About = () => {
+    return (
+        <div>
+            <Header />
+            <h1>About</h1>
+            <Footer />
+        </div>
+    );
+};
+
+export default About;
