6d2bb0cb03e7efd99f5a774b5b17ffac1372fb8a
Alson Shareef <alson.shareef@gmail.com>
2019-08-24 16:08:41 +0800

    Created a content div to hold everything but Footer and applied respective styles from Layout Sass Module.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import '../../styles/index.scss';
 
+import layoutStyles from './Layout.module.scss';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
 const Layout = props => {
     return (
-        <div>
-            <Header />
-            {props.children}
+        <div className={layoutStyles.container}>
+            <div className={layoutStyles.content}>
+                <Header />
+                {props.children}
+            </div>
             <Footer />
         </div>
     );
